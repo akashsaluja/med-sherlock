7242e9f95316a4382541c67b2b2ea37036355ff0
akashdeep.saluja <akashdeep1412@gmail.com>
2017-05-21 10:47:36 +0530

    Added tests screen, multiple tests allowed

--- src/public/test.controller.js
+++ src/public/test.controller.js
@@ -2,14 +2,16 @@
     angular.module('public')
     .controller('TestsController', TestsController);
 
-    TestsController.$inject = ['NewPatientService' ,'$scope'];
-    function TestsController(NewPatientService, $scope) {
+    TestsController.$inject = ['NewPatientService' ,'TestsService', '$scope'];
+    function TestsController(NewPatientService, TestsService, $scope) {
         var $ctrl = this;
         console.log('Hey');
         $ctrl.patientId = '';
         $ctrl.patientMessage = '';
         $ctrl.items = ['Akash', 'Saluja'];
         $ctrl.patient = '';
+        $ctrl.noOfTests = 0;
+        $ctrl.finalMessage = '';
 
         $ctrl.patientCallback = function(docs) {
             if(docs.length != 0) {
@@ -35,6 +37,33 @@
                 NewPatientService.getPatients(ids, $ctrl.patientCallback);
             }
             
+        }
+
+        $ctrl.submit = function() {
+            console.log($ctrl.isSugar);
+             $ctrl.noOfTests = 0;
+             
+            if($ctrl.isSugar) {
+                 $ctrl.noOfTests++;
+            }
+            if($ctrl.isHbsag) {
+                $ctrl.noOfTests++;
+            }
+            if($ctrl.isSugar) {
+                TestsService.addTest($ctrl.patient.id, 'Sugar', $ctrl.amount/$ctrl.noOfTests, $ctrl.testSaveCallback);
+            }
+            if($ctrl.isHbsag) {
+                TestsService.addTest($ctrl.patient.id, 'Sugar', $ctrl.amount/$ctrl.noOfTests, $ctrl.testSaveCallback);
+            }
+        }
+
+        $ctrl.testSaveCallback = function(testName) {
+            $ctrl.noOfTests--;
+            console.log($ctrl.noOfTests);
+            if($ctrl.noOfTests == 0) {
+                $ctrl.finalMessage = 'Tests Saved';
+                $scope.$digest();
+            }
         }
 
         
